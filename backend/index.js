f046eabd2f26c7d86c40c2fe3d8b78bbaff7e34b
anshika singh <anshikas00449@gmail.com>
2026-04-21 07:17:46 +0530

    Update CORS origin to production URL

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://stayeasy-d17y.onrender.com",
     credentials:true
 }))
 
